5db0d9de1bd27f30bd016199a1807b7fd68192fe
oimomiox <i.m.c_dr-ox-beat@i.softbank.jp>
2020-07-10 20:28:50 +0900

    Initial Commit

---
+++ Suzukisyoten_Kadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Suzukisyoten_Kadai
+//
+//  Created by 井本大貴 on 2020/07/10.
+//  Copyright © 2020 OIMO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
